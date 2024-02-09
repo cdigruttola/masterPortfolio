ef09fa7868f8a8fabb06f9c18ee416a477aa5445
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2024-02-09 11:05:00 +0100

    fix icon

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -17,19 +17,35 @@ export default function socialMedia(props) {
   return (
     <div className="social-media-div">
       {socialMediaLinks.map((media, i) => {
-        return (
-          <a
-            key={i}
-            href={media.link}
-            className={`icon-button`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconWrapper {...media} {...props}>
-              <i className={`fab ${media.fontAwesomeIcon}`}></i>
-            </IconWrapper>
-          </a>
-        );
+        if (media.isBrand) {
+          return (
+            <a
+              key={i}
+              href={media.link}
+              className={`icon-button`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconWrapper {...media} {...props}>
+                <i className={`fab ${media.fontAwesomeIcon}`}></i>
+              </IconWrapper>
+            </a>
+          );
+        } else {
+          return (
+            <a
+              key={i}
+              href={media.link}
+              className={`icon-button`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconWrapper {...media} {...props}>
+                <i className={`${media.fontAwesomeIcon}`}></i>
+              </IconWrapper>
+            </a>
+          );
+        }
       })}
     </div>
   );
